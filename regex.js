dc41f97cf96cef06d542208f74b903586db8aa75
Caleb <calebcoecode@gmail.com>
2020-12-31 18:10:18 -0700

    Initial commit

---
+++ regex.js
@@ -0,0 +1,80 @@
+const str = "The fat cat ran dow the street. It was searching for a mouse to eat."
+
+// + will match one or more e's
+const reg1 = /e+/g;
+
+// will match all e's as well as any ea. The ? after the 'a' marks it as an optional match
+const reg2 = /ea?/g;
+
+// * is a combo of ? and +. It will optionally match one or more 'e'
+const reg3 = /re*/g;
+
+// . is a wildcard that will match anything. This will match fat, cat and eat
+const reg4 = /.at/g;
+
+// the \ will cancel anything after it so it is not treated as a special character.
+// in this case it is searching for a period instead of using the wildcard
+const reg5 = /\./g;
+
+// \w will match any word character (alphanumeric and underscore)
+// using \W will do the inverse and find anything that is NOT a word character
+// \s will find whitespace and \S will do the inverse
+const reg6 = /\w/g;
+
+// find words that have minimum 4 or max 5 word characters in a row
+// searching will return 2 matches. first is searc and second is hing
+const reg7 = /\w{4,5}/g;
+
+// [fc] will search for either an f or c before at so fat or cat.
+// [a-zA-Z] will search if any letter precedes at so fat, cat and eat will match
+// [0-9] it works with a range of numbers as well 
+const reg8 = /[a-z]at/g;
+
+// will search for either The or the
+const reg9 = /(t|T)he/g;
+
+// will match tre and et in street
+const reg10 = /(t|e|r){2,3}/g;
+
+// ^ searches only the beginning of a line. Will match the very first character T
+const reg11 = /^T/g;
+
+// the $ sybol will do the inverse of the ^ by searching the very end
+const reg12 = /\.$/g;
+
+// look behind (?<=) will match to anything that is PRECEDED by the criteria.
+// in this case the wildcard '.' will match two whitespaces that have the word 'the' before them\
+// the ! instead of = is the inverse and will match everything except the two whitespaces
+const reg13 = /(?<=[tT]he)./g;
+const reg14 = /(?<![tT]he)./g;
+
+// look ahead is the same as look behind just without the <
+// grab any character that is follow by at. 'f', 'c' and 'e' in this case
+// can also be inverted by replacing '=' with '!'
+const reg15 = /.(?=at)/g;
+
+///// FIND PHONE NUMBERS /////
+// 1234567890
+let regEx = /\d{10}/g;
+
+// 1234567890
+// 123-456-7890
+regEx = /\d{3}-?\d{3}-?\d{4}/g;
+
+// 1234567890
+// 123-456-7890
+// 123 456 7890
+regEx = /(\d{3})[ -]?(\d{3})[ -]?(\d{4})/g;
+
+// 1234567890
+// 123-456-7890
+// 123 456 7890
+// (123) 456-7890
+regEx = /\(?(\d{3})\)?[ -]?(\d{3})[ -]?(\d{4})/g;
+
+// 1234567890
+// 123-456-7890
+// 123 456 7890
+// (123) 456-7890
+// +1 123 456 7890
+regEx = /(?:(\+1)[ -]?)\(?(\d{3})\)?[ -]?(\d{3})[ -]?(\d{4})/g;
